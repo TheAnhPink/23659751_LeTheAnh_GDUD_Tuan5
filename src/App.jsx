b936ead6c1d2b6a7853ec4d67313b27e80869d22
H82M19\Student <Student>
2026-03-30 08:52:09 +0700

    Cau 3 chua xong

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About'
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Link, Outlet} from "react-router-dom"
 import Not_found from './components/Not_found'
+import Products from './components/DanhSachSanPham/Products'
+import ProductItem from "./components/DanhSachSanPham/ProductItem"
 
 function App() {
 
@@ -14,12 +16,18 @@ function App() {
         <Link to="/">Home </Link> |
         <Link to="/about">About </Link> |
         <Link to="/contact">Contact </Link>
+        <Link to="/products">Products</Link>
       </nav>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="*" element={<Not_found/>}/>
+        
+          <Route path="/products" element={<Products/>}>
+          <Route path="/products:id" element={ProductItem}/>
+
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/DanhSachSanPham/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter ,Link,Route,Routes} from 'react-router-dom'
+import Product from "./ProductItem"
+
+function Products() {
+  return (
+    <div>
+      
+
+    </div>
+  )
+}
+
+export default Products
